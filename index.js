cd9ad20fd5341f1300c709def671ddd2a3c4b25f
Tess <tess.merrill25@gmail.com>
2023-07-03 20:18:52 -0600

    complete dom elements lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// Make sure you remove the <main> with id 'main'
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Tess is the champion";
+document.body.append(newHeader);
+
+// Make sure your <h1> with id 'victory' includes the text \"is the champion\" inside it
